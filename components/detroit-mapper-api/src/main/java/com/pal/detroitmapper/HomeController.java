fac82800452d11669d1db80977fc08515a890356
Vibhor Kainth <vibhor.kainth@perficient.com>
2019-06-13 15:32:11 -0700

    Add web GUI

--- components/detroit-mapper-api/src/main/java/com/pal/detroitmapper/HomeController.java
+++ components/detroit-mapper-api/src/main/java/com/pal/detroitmapper/HomeController.java
@@ -1,4 +1,36 @@
 package com.pal.detroitmapper;
 
+import com.pal.detroitmapper.restaurantsapi.RestaurantFixtures;
+import com.pal.detroitmapper.restaurantsapi.RestaurantInfo;
+import com.pal.detroitmapper.restaurantsapi.RestaurantsClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
 public class HomeController {
+
+    private final RestaurantsClient restaurantsClient;
+    private final RestaurantFixtures restaurantFixtures;
+
+    public HomeController(RestaurantsClient restaurantsClient, RestaurantFixtures restaurantFixtures) {
+        this.restaurantsClient = restaurantsClient;
+        this.restaurantFixtures = restaurantFixtures;
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/setup")
+    public String setup(Map<String, Object> model) {
+        for (RestaurantInfo info: restaurantFixtures.load()) {
+            restaurantsClient.addRestaurant(info);
+        }
+        model.put("restaurants", restaurantsClient.getRestaurants());
+
+        return "setup";
+    }
 }
--- components/restaurants/src/main/java/com/pal/detroitmapper/restaurants/RestaurantController.java
+++ components/restaurants/src/main/java/com/pal/detroitmapper/restaurants/RestaurantController.java
@@ -25,17 +25,24 @@ public class RestaurantController {
     }
 
     @GetMapping
-    public List<Restaurant> list() {
-        return this.repository.findAll();
+    public List<Restaurant> find(@RequestParam(name = "field", required = false) String field,
+                            @RequestParam(name = "key", required = false) String key,
+                            @RequestParam(name = "start", required = false) Integer start,
+                            @RequestParam(name = "pageSize", required = false) Integer pageSize) {
+        if (field != null && key != null) {
+            return repository.findRange(field, key, start, pageSize);
+        } else if (start != null && pageSize != null) {
+            return repository.findAll(start, pageSize);
+        }
+        return repository.getRestaurants();
     }
 
-    @GetMapping("/{restroId}")
-    public Restaurant getOne(@PathVariable Long restroId) {
-        return this.repository.find(restroId);
-    }
-
-    @PutMapping
-    public void updateRestro(@RequestBody Restaurant restaurant) {
-        this.repository.updateRestro(restaurant);
+    @GetMapping("/count")
+    public int count(@RequestParam(name = "field", required = false) String field,
+                     @RequestParam(name = "key", required = false) String key) {
+        if (field != null && key != null) {
+            return repository.count(field, key);
+        }
+        return repository.countAll();
     }
 }
